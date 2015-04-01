d9a6670ddae8ae38f0af08d9feb6c7a80dbdb98c
HarriKnox <harri.alpha96@gmail.com>
2015-04-01 15:47:04 -0700

    Moved functionality from `main.py` to `Ball` class

--- lab1/methods/ball.py
+++ lab1/methods/ball.py
@@ -20,3 +20,22 @@ class Ball:
          pygame.Rect(self.x - self.radius, self.y - self.radius,
             self.radius * 2, self.radius * 2)) 
 
+
+
+   def can_move_horizontal(self, width):
+      return ((self.dx > 0 and self.x + self.radius + self.dx < width) or
+            (self.dx < 0 and self.x - self.radius + self.dx >= 0))
+
+
+   def can_move_vertical(self, height):
+      return ((self.dy > 0 and self.y + self.radius + self.dy < height) or
+            (self.dy < 0 and self.y - self.radius + self.dy >= 0))
+
+
+   def move(self, width, height, selfs):
+      if not self.can_move_horizontal(width):
+         self.dx *= -1
+      if not self.can_move_vertical(height):
+         self.dy *= -1
+      self.x += self.dx
+      self.y += self.dy
